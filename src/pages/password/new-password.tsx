926bebb72dcadb7841df3c4acd72c4a94b7ef656
Maskalenchik Vlas <vlasmaskalenchik1998@gmail.com>
2023-08-06 21:13:29 +0300

    added new password page

---
+++ src/pages/password/new-password.tsx
@@ -0,0 +1,63 @@
+import { ChangeEvent, useState } from "react";
+import password from "../../assets/images/passwrod-new.png";
+import styles from "./password.module.scss";
+import { useAppDispatch } from "../../redux/reduxUtils";
+import { handlerDeleteHint } from "../../utils/functionutils";
+import { HintModal } from "../../components/modals/hintModal/hint-modal";
+
+export const NewPassword = () => {
+  const dispatch = useAppDispatch();
+  const [passwordOne, setPasswordOne] = useState("");
+  const [passwordTwo, setPasswordTwo] = useState("");
+
+  const handlerState =
+    (state: (value: string) => void) => (e: ChangeEvent<HTMLInputElement>) => {
+      state(e.target.value);
+    };
+
+  const handlerAccept = () => {
+    if (passwordOne.length < 3 || passwordTwo.length < 3) {
+      handlerDeleteHint(
+        "Password should be more than 3 characters",
+        dispatch,
+        "error"
+      );
+      return;
+    }
+    if (passwordOne !== passwordTwo) {
+      handlerDeleteHint(
+          "Passwords are different",
+          dispatch,
+          "error"
+      );
+      return
+    }
+  };
+
+  return (
+    <>
+      <HintModal />
+      <section className={styles.newPassword}>
+        <section className={styles.newPassword_section}>
+          <h1>Come up with a new password</h1>
+          <img src={password} alt="" />
+          <section className={styles.newPassword_section_forms}>
+            <input
+              value={passwordOne}
+              onChange={handlerState(setPasswordOne)}
+              type="password"
+              placeholder="new password"
+            />
+            <input
+              value={passwordTwo}
+              onChange={handlerState(setPasswordTwo)}
+              type="password"
+              placeholder="repeat password"
+            />
+          </section>
+          <button onClick={handlerAccept}>Accept</button>
+        </section>
+      </section>
+    </>
+  );
+};
