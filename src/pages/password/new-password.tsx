e404f0a12dd659f0cae22c5aa0f0933ffbb4abea
Maskalenchik Vlas <vlasmaskalenchik1998@gmail.com>
2023-08-13 20:10:15 +0300

    made new-password

--- src/pages/password/new-password.tsx
+++ src/pages/password/new-password.tsx
@@ -1,17 +1,24 @@
 import { ChangeEvent, useState } from "react";
 import password from "../../assets/images/passwrod-new.png";
 import styles from "./password.module.scss";
+import styles2 from "../registration/registration.module.scss";
 import { useAppDispatch } from "../../redux/reduxUtils";
 import { handlerDeleteHint } from "../../utils/functionutils";
 import { HintModal } from "../../components/modals/hintModal/hint-modal";
 import { fetchNewPassword } from "../../redux/authReducer";
 import { useParams } from "react-router-dom";
+import eye from "../../assets/images/eye.png";
+import eye2 from "../../assets/images/eyep.png";
+import newPassword from "../../assets/images/passwrod-new.png";
 
 export const NewPassword = () => {
   const dispatch = useAppDispatch();
-  const { id = '' } = useParams();
+  const { id = "" } = useParams();
   const [passwordOne, setPasswordOne] = useState("");
   const [passwordTwo, setPasswordTwo] = useState("");
+
+  const [isVisible, setVisible] = useState<boolean>(false);
+  const changeVisible = () => setVisible(!isVisible);
 
   const handlerState =
     (state: (value: string) => void) => (e: ChangeEvent<HTMLInputElement>) => {
@@ -40,20 +47,56 @@ export const NewPassword = () => {
       <section className={styles.newPassword}>
         <section className={styles.newPassword_section}>
           <h1>Come up with a new password</h1>
-          <img src={password} alt="" />
+          <img className={styles.newPassword_section_image} src={newPassword} alt="newPassword" />
           <section className={styles.newPassword_section_forms}>
-            <input
-              value={passwordOne}
-              onChange={handlerState(setPasswordOne)}
-              type="password"
-              placeholder="new password"
-            />
-            <input
-              value={passwordTwo}
-              onChange={handlerState(setPasswordTwo)}
-              type="password"
-              placeholder="repeat password"
-            />
+            <section className={styles2.registration_fill_field}>
+              <div>
+                <label
+                  style={{ visibility: passwordOne ? "visible" : "hidden" }}
+                >
+                  Password
+                </label>
+                <input
+                  name="password"
+                  autoComplete="current-password webauthn"
+                  type={isVisible ? "text" : "password"}
+                  value={passwordOne}
+                  onChange={handlerState(setPasswordOne)}
+                  placeholder={"Password"}
+                />
+              </div>
+              <img
+                onClick={changeVisible}
+                src={isVisible ? eye2 : eye}
+                alt={password}
+                role="password-picture"
+                title="password"
+              />
+            </section>
+            <section className={styles2.registration_fill_field}>
+              <div>
+                <label
+                  style={{ visibility: passwordTwo ? "visible" : "hidden" }}
+                >
+                  Password
+                </label>
+                <input
+                  name="password"
+                  autoComplete="current-password webauthn"
+                  type={isVisible ? "text" : "password"}
+                  value={passwordTwo}
+                  onChange={handlerState(setPasswordTwo)}
+                  placeholder={"Repeat password"}
+                />
+              </div>
+              <img
+                onClick={changeVisible}
+                src={isVisible ? eye2 : eye}
+                alt={password}
+                role="password-picture"
+                title="password"
+              />
+            </section>
           </section>
           <button onClick={handlerAccept}>Accept</button>
         </section>
